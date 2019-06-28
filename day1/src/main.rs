c992bb0749cb264803c14936b8242f8aa52c7039
Jordan Ayvazian <jaayvazian@gmail.com>
2019-06-28 03:34:42 -0700

    added some comments and fixed a variable name

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,15 +6,23 @@ fn main() {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
     for input in contents.lines() {
-        let modifer = &input[0..1];
+
+        // Get the + or - mod
+        let modifier = &input[0..1];
+
+        // change our string from the file into an int so we can make it do math
         let value = &input[1..].parse::<i32>().unwrap();
-        if modifer == String::from("+"){            
+
+        // if this modifier is the + char add the value if its anything else subtract the value
+        if modifier == String::from("+"){            
             freq += value;
         }
         else {
             freq -= value;
         }
     }
+
+    // Ding ding the answer!
     println!("{}", freq);    
 
 }
